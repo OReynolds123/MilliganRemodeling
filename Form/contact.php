1aee1f9a160ea9c83fbd0c65ba5c076d9b75d8df
OReynolds123 <owenreynolds4@gmail.com>
2021-09-27 10:53:59 -0400

    Update contact.php

--- Form/contact.php
+++ Form/contact.php
@@ -1,48 +1,9 @@
-<?php
-if ($_POST) {
-    $name = "";
-    $email = "";
-    $message = "";
-    $email_body = "<div>";
-    $subject = "Contact Form from Website";
-    $recipient = "owenreynolds4@uga.edu"
-     
-    if(isset($_POST['name'])) {
-        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-        $email_body .= "<div>
-                           <label><b>Name:</b></label>&nbsp;<span>".$name."</span>
-                        </div>";
-    }
+<html>
+<body>
 
-    if(isset($_POST['email'])) {
-        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
-        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-        $email_body .= "<div>
-                           <label><b>Email:</b></label>&nbsp;<span>".$email."</span>
-                        </div>";
-    }
-     
-    if(isset($_POST['message'])) {
-        $message = htmlspecialchars($_POST['message']);
-        $email_body .= "<div>
-                           <label><b>Message:</b></label>
-                           <div>".$message."</div>
-                        </div>";
-    }
-     
-    $email_body .= "</div>";
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your message is: <?php echo $_POST["message"]; ?>
 
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $email . "\r\n";
-     
-    if(mail($recipient, $subject, $email_body, $headers)) {
-        echo "<p>Thank you for contacting me, $name. You will get a reply within 24 hours.</p>";
-    } else {
-        echo '<p>I am sorry but the email did not go through.</p>';
-    }
-     
-} else {
-    echo '<p>Something went wrong</p>';
-}
-?>
+</body>
+</html>
