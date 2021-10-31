0d80330b3d8c9ece8c43a7d10a8c208336d88daf
OReynolds123 <owenreynolds4@gmail.com>
2021-10-31 12:07:45 -0400

    Update

--- javascripts/contactForm.php
+++ javascripts/contactForm.php
@@ -6,13 +6,13 @@
 
     $conn = new mysqli($config['srvr'], $config['user'], $config['pass'], $config['data']);
     if (mysqli_connect_error()) {
-        header("Location: ../index.html?success=0#contact");
+        header("Location: ../pages/contact.html?success=0#contact");
     } else {
         $sql = "INSERT INTO contact (id, name, email, message) values ('0', '$name', '$email', '$message')";
         if ($conn->query($sql)){
-            header("Location: ../index.html?success=1#contact");
+            header("Location: ../pages/contact.html?success=1#contact");
         } else {
-            header("Location: ../index.html?success=0#contact");
+            header("Location: ../pages/contact.html?success=0#contact");
         }
         $conn->close();
     }
