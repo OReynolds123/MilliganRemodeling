e428823b521bc414156ef8209327c1caf6bffaac
OReynolds123 <owenreynolds4@gmail.com>
2021-12-06 13:00:12 -0500

    internal comments

--- pages/customer.php
+++ pages/customer.php
@@ -1,22 +1,26 @@
 <?php
+    # Check for sessions
     session_start();
-    if (empty($_SESSION["username"])) {
+    if (empty($_SESSION["username"])) { # Redirect to login page if no sessions
         header("Location: ../pages/login.html");
         exit;
     }
-    if ($_SESSION['employee'] == TRUE) {
+    if ($_SESSION['employee'] == TRUE) { # Redirect to employee page if employee user
         header("Location: ../pages/employees.php");
         exit;
     }
 
-    $config = parse_ini_file("../javascripts/.ht.ini");
-    $conn = new mysqli($config['srvr'], $config['user'], $config['pass'], $config['data']);
-    if (mysqli_connect_error()) {
+    # Connect to the database
+    $config = parse_ini_file("../javascripts/.ht.ini"); # Parse the secure information from this hidden file
+    $conn = new mysqli($config['srvr'], $config['user'], $config['pass'], $config['data']); # Connect to the database
+    if (mysqli_connect_error()) { # On error, redirect to an error page
         header("Location: error.html?success=0");
         exit;
     } else {
+        # Get all of the current customer info from the database
         $sql1 = "SELECT `id`,`name`,`email`,`phone`,`card`,`orders` FROM `customer` WHERE `username`='" . $_SESSION["username"] . "' AND `password`='" . $_SESSION["password"] . "'";
         $rslt1 = $conn->query($sql1);
+        # Store the information in session if not empty
         if (mysqli_num_rows($rslt1) < 1) {
             $_SESSION["id"] = "";
             $_SESSION["username"] = "";
@@ -38,8 +42,10 @@
             $_SESSION["orders"] = $row1['orders'];
         }
 
+        # Get all of the current order info from the database as it relates to the customer
         $sql2 = "SELECT `name`,`progress`,`description`,`cost`,`isPaid` FROM `orders` WHERE `id`='" . $_SESSION["orders"] . "'";
         $rslt2 = $conn->query($sql2);
+        # Store the information in session if not empty
         if (mysqli_num_rows($rslt2) < 1) {
             $_SESSION["orderName"] = "";
             $_SESSION["prog"] = "";
@@ -122,11 +128,11 @@
     <div class="pageLanding">
         <div class="pageLandingDiv">
             <div style="position:relative;">
-                <div class="customerLandingSuccessBanner"></div>
+                <div class="customerLandingSuccessBanner"></div> <!-- Banner to show response information -->
             </div>
 
             <div class="pageLandingTitleDiv">
-                <span class="pageLandingTitle">Customer</span>
+                <span class="pageLandingTitle">Customer</span> <!-- Title -->
             </div>
             
             <div class="customerLandingDiv">
@@ -135,11 +141,11 @@
                     <div class="customerLandingLeft">
                         <div style="font-size:25px">Project:</div>
                         <div class="customerLandingCustomerParent">
-                            <div onmousedown="customerLandingProfilePress(this);" class="customerLandingCustomerTitle"><?php echo $_SESSION['orderName']; ?>
+                            <div onmousedown="customerLandingProfilePress(this);" class="customerLandingCustomerTitle"><?php echo $_SESSION['orderName']; ?> <!-- Project expandable button -->
                                 <svg class="up" viewBox="0 0 24 24"><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path></svg>
                                 <svg class="down" viewBox="0 0 24 24"><path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"></path></svg>
-                            </div>
-                            <div class="customerLandingCustomerChild">
+                            </div> 
+                            <div class="customerLandingCustomerChild"> <!-- Customer order information -->
                                 Description: <?php echo $_SESSION["desc"]; ?><br>
                                 Progress: <?php echo $_SESSION["prog"]; ?>%<br>
                                 Cost: $<?php echo $_SESSION["cost"]; ?><br>
@@ -151,11 +157,11 @@
                     <div class="customerLandingLeft hide">
                         <div style="font-size:25px">Profile:</div>
                         <div class="customerLandingCustomerParent">
-                            <div onmousedown="customerLandingProfilePress(this);" class="customerLandingCustomerTitle"><?php echo $_SESSION['name']; ?>
+                            <div onmousedown="customerLandingProfilePress(this);" class="customerLandingCustomerTitle"><?php echo $_SESSION['name']; ?> <!-- Profile expandable button -->
                                 <svg class="up" viewBox="0 0 24 24"><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path></svg>
                                 <svg class="down" viewBox="0 0 24 24"><path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"></path></svg>
                             </div>
-                            <form class="customerLandingCustomerChild" method="POST" action="../javascripts/customerForm.php">
+                            <form class="customerLandingCustomerChild" method="POST" action="../javascripts/customerForm.php"> <!-- Customer profile information (allows customer to read and update their information) -->
                                 Full Name: <input class="customerLandingCustomerChildInput" type="text" name="editCustomerName" placeholder="<?php echo $_SESSION["name"]; ?>"><br>
                                 Username: <input class="customerLandingCustomerChildInput" type="text" name="editCustomerUser" placeholder="<?php echo $_SESSION["username"]; ?>"><br>
                                 Password: <input class="customerLandingCustomerChildInput" type="password" name="editCustomerPass" placeholder="<?php echo $_SESSION["password"];?>"><br>
@@ -170,7 +176,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="customerLandingRightDiv">
+                <div class="customerLandingRightDiv"> <!-- Customer navigation bar -->
                     <a onmousedown="customerBtnPress(0);" class="customerLandingRightBtn active">
                         <svg viewBox="0 0 576 512"><path d="M573.65 200.92l-11.34-11.31c-3.13-3.12-8.21-3.12-11.34 0l-17.01 16.97-52.98-52.86c5.64-21.31.36-44.9-16.39-61.61l-45.36-45.25C387.92 15.62 346.88 0 305.84 0c-41.04 0-82.09 15.62-113.4 46.86l68.66 68.5c-4.42 16.71-1.88 34.7 7.4 49.8L21.47 395.26c-27.95 26.04-28.71 70.01-1.67 96.99C33.02 505.44 50.32 512 67.59 512c18.05 0 36.09-7.17 49.42-21.42l233.13-249.14c12.84 5.06 26.84 6.3 40.12 2.8l52.98 52.86-17.01 16.97a7.985 7.985 0 0 0 0 11.31l11.34 11.31c3.13 3.12 8.21 3.12 11.34 0l124.74-124.45a7.997 7.997 0 0 0 0-11.32zM93.57 468.74C86.78 476 77.55 480 67.59 480c-9.48 0-18.4-3.69-25.11-10.38-6.87-6.86-10.57-15.97-10.4-25.67.17-9.7 4.17-18.68 11.28-25.3l246.37-229.47 33.85 33.77L93.57 468.74zm372.35-194.28l-52.98-52.85-13.04-13.01-17.83 4.7c-11.3 2.98-22.84-.03-30.87-8.04l-51.03-50.91c-8.03-8.01-11.04-19.53-8.06-30.8l4.71-17.79-13.04-13.01-43.14-43.05c19.54-11.54 41.9-17.7 65.2-17.7 34.27 0 66.48 13.32 90.72 37.49l45.36 45.26c8.03 8.01 11.04 19.53 8.06 30.8l-4.71 17.79 13.04 13.01 52.98 52.86-45.37 45.25z"></path></svg>
                         <div>Project</div>
@@ -194,6 +200,7 @@
 <script>
     init();
     function init() {
+        // If the url has a ? in it, the user was redirected here with some sort of message
         if (window.location.href.includes("?")) {
             var checkurl = window.location.href.split("?")[1].split("#")[0];
             if (checkurl == "success=1") {
@@ -209,7 +216,9 @@
             }
         }
     }
+    // These functions control the html customer button elements
     function customerLandingProfilePress(elem) {
+        // Shows the hidden information
         elem.parentElement.classList.toggle("selected");
     }
     function customerBtnPress(num) {
