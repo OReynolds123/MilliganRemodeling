8b4ce2fd6f6bdabc9aafcbfb5b07f31f616d9016
OReynolds123 <owenreynolds4@gmail.com>
2021-12-06 12:34:26 -0500

    Database

--- milliganremodeling.sql
+++ milliganremodeling.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2021 at 08:01 PM
+-- Generation Time: Dec 06, 2021 at 05:22 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
